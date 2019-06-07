37bfbfd28ee60f0b2944387153c158c9002b1527
themachine <themachine@thegnet.tk>
2019-06-07 14:52:59 -0700

    check if mount is successful and notify if not

--- system/keepassauto.sh
+++ system/keepassauto.sh
@@ -2,6 +2,20 @@
 # mount webdav keepass folder and start keepassxc
 # requires dav2fs and the davfs mount set up for your user in /etc/fstab
 mount $HOME/.keepass/
-keepassxc
-wait $!
-fusermount -u $HOME/.keepass/
+mountcode="$?"
+if [ "$mountcode" -ne "0" ]
+	then
+		dbus-send --type=method_call --dest='org.freedesktop.Notifications' \
+        /org/freedesktop/Notifications org.freedesktop.Notifications.Notify \
+        string:'[APPLICATION]' \
+        uint32:1 string:'[ICON]' \
+        string:'' \
+        string:"Error mounting davfs" \
+        array:string:'' \
+        dict:string:string:'','' \
+        int32:3000
+	else
+		keepassxc
+		wait $!
+		fusermount -u $HOME/.keepass/
+fi
