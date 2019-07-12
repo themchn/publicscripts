8ada9ef43de9faf89020313cd7c308c71e57d57b
Gunnar Cameron <gcameron@teo-op04.omnis.com>
2019-07-12 10:45:22 -0700

    curl for specific vars and removed emoji case

--- irc/irc_weather.sh
+++ irc/irc_weather.sh
@@ -4,22 +4,7 @@
 location="$1"
 
 # create array from wttr.in output
-readarray -t weather_vars < <( curl -s -N wttr.in/"$location"?0?Q?T | cut -c 16- | sed -e 's/ *$//' -e "s/\x95 *//g" -e 's/^ *//g' )
+readarray -t weather_vars < <( curl -s -N curl -s -N wttr.in/"$location"?format=%c+%C%7C%t%7C%h%7C%p%7C%w%7C%m | sed -e 's/|/\n/g' -e 's/%/\%\%/g'
 
-# add emoji of weather condition
-case "${weather_vars[0]}" in
-Sunny*|Clear*)
-    weather_vars[0]=$(echo -n "☀️ ${weather_vars[0]}")
-    ;;
-Light*Rain*)
-    weather_vars[0]=$(echo -n "🌦️ ${weather_vars[0]}")
-    ;;
-Overcast*)
-    weather_vars[0]=$(echo -n "☁️ ${weather_vars[0]}")
-    ;; 
-Partly*cloudy*)
-    weather_vars[0]=$(echo -n "⛅ ${weather_vars[0]}")
-    ;;    
-esac
 # print it out
- printf "\\x02Conditions:\\x0f ${weather_vars[0]} \\x02• Temp:\\x0f ${weather_vars[1]} \\x02• Wind:\\x0f ${weather_vars[2]} \\x02• Visibility:\\x0f ${weather_vars[3]} \\x02• Rainfall:\\x0f ${weather_vars[4]}\n"
+printf "\\x02Conditions:\\x0f ${weather_vars[0]} \\x02• Temp:\\x0f ${weather_vars[1]} \\x02• Humidity:\\x0f ${weather_vars[2]} \\x02• Rainfall:\\x0f ${weather_vars[3]} \\x02• Wind:\\x0f ${weather_vars[4]} \\x02• Moon phase:\\x0f ${weather_vars[5]}\n"
