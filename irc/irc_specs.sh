0d389daacb2bb612aeeda00abb3f4f1d07db2b28
themchn <>
2019-11-15 08:49:26 -0700

    added threads and cores for cpu

--- irc/irc_specs.sh
+++ irc/irc_specs.sh
@@ -21,6 +21,8 @@ fi
 os=$(grep PRETTY_NAME /etc/os-release | cut -d'"' -f2)
 cpu=$(grep "model name" /proc/cpuinfo | uniq | cut -d":" -f2 | sed -e 's/^ //' -e 's/ \+/ /g')
 sockets=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+cores=$(grep cores /proc/cpuinfo | uniq | grep -Eo '[0-9]+')
+threads=$(expr "$(grep processor /proc/cpuinfo | wc -l)" / "$sockets")
 memory=$(free -h | awk '/Mem/{print ""$2" Total ("$4" Free)"}')
 # add together bytes of all attached disks
 storage=$(sum=0; while read bytes ; do sum=$(expr $bytes + $sum) ; done <<< $(lsblk -b | grep disk | awk '{print $4}') ; echo $sum)
@@ -46,13 +48,13 @@ case "$formatting" in
 plaintext|"")
     bold=$(tput bold)
     normal=$(tput sgr0)
-    echo "${bold}OS:${normal} $os ${bold}• CPU:${normal} $cpu ${bold}• Sockets:${normal} $sockets ${bold}• Memory:${normal} $memory ${bold}• Storage:${normal} $storage •${bold} Uptime:${normal} $uptime"
+    echo "${bold}OS:${normal} $os ${bold}• CPU:${normal} $cpu ${bold}• Cores:${normal} $cores ${bold}• Threads:${normal} $threads ${bold}• Sockets:${normal} $sockets ${bold}• Memory:${normal} $memory ${bold}• Storage:${normal} $storage •${bold} Uptime:${normal} $uptime"
     ;;
 weechat-direct)
-    printf "\\x02OS:\\x0f $os \\x02• CPU:\\x0f $cpu \\x02• Sockets:\\x0f $sockets \\x02• Memory:\\x0f $memory \\x02• Storage:\\x0f $storage \\x02• Uptime:\\x0f $uptime"
+    printf "\\x02OS:\\x0f ""$os"" \\x02• CPU:\\x0f ""$cpu"" \\x02• Cores:\\x0f ""$cores"" \\x02• Threads:\\x0f ""$threads"" \\x02• Sockets:\\x0f ""$sockets"" \\x02• Memory:\\x0f ""$memory"" \\x02• Storage:\\x0f ""$storage"" \\x02• Uptime:\\x0f ""$uptime""\n"
     ;;
 weechat-paste)
-    echo "/exec -o printf \"\\x02OS:\\x0f $os \\x02• CPU:\\x0f $cpu \\x02• Sockets:\\x0f $sockets \\x02• Memory:\\x0f $memory \\x02• Storage:\\x0f $storage \\x02• Uptime:\\x0f $uptime"\"
+    echo "/exec -o printf \"\\x02OS:\\x0f ""$os"" \\x02• CPU:\\x0f ""$cpu"" \\x02• Cores:\\x0f ""$cores"" \\x02• Threads:\\x0f ""$threads"" \\x02• Sockets:\\x0f ""$sockets"" \\x02• Memory:\\x0f ""$memory"" \\x02• Storage:\\x0f ""$storage"" \\x02• Uptime:\\x0f ""$uptime""\n"\"
     ;;
 *)
     echo "Unrecognized format"
